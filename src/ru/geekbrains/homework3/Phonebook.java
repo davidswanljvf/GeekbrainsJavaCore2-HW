bdd9708e76bf0325387727b99dd6508b307d0eeb
DavidSwan <ljvf@mail.ru>
2019-04-08 13:34:47 +0300

    HomeWork-3

---
+++ src/ru/geekbrains/homework3/Phonebook.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.homework3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Phonebook {
+    private HashMap<String, ArrayList<String>> book;
+    public Phonebook(){
+        this.book = new HashMap<>();
+    }
+    public void add(String surname, String phone){
+        if(!book.containsKey(surname)){
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add(phone);
+            book.put(surname, arrayList);
+        }else book.get(surname).add(phone);
+    }
+    public ArrayList<String> get(String surname){
+        return book.get(surname);
+    }
+}
